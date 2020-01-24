c9a7109ab2f92d23bd503d24e2a6fd6b71c219ee
NetanelShimoni <57719538+NetanelShimoni@users.noreply.github.com>
2020-01-24 16:02:19 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #define NUM_LETTERS ((int)26)
 #define END_CHAR 97
 #define line_size 1024
-#define word_size 256
+#define word_size 12
 
 
 typedef enum {FALSE=0 ,TRUE=1} boolean;
@@ -44,13 +44,13 @@ node* getNode(void)
     return root;
 }
 //add a word to the trie
-void insert(node *root,  char* key)
+void insert(node **root,  char* key)
 {
     int level;
     int length = strlen(key);
     int index;
 
-   node *pCrawl = root;
+   node *pCrawl = *root;
 
     for (level = 0; level < length; level++)
     {
@@ -81,6 +81,15 @@ int Stop(node *current){
 void printR(node *root, char *str, int index)
 {
 
+    for (int i = NUM_LETTERS - 1; i >= 0; i--)
+    {
+        if (root->children[i])
+        {
+            str[index] = root->children[i]->letter;
+            printR(root->children[i], str, index + 1);
+        }
+    }
+
     if (Stop(root) == FALSE)
     {
         str[index] = '\0';
@@ -94,14 +103,6 @@ void printR(node *root, char *str, int index)
         printf("%s\t%ld\n", str, root->count);
     }
 
-    for (int i = NUM_LETTERS - 1; i >= 0; i--)
-    {
-        if (root->children[i])
-        {
-            str[index] = root->children[i]->letter;
-            printR(root->children[i], str, index + 1);
-        }
-    }
 }
 
 ///print the words in trie sorted by a declining lexicographic order
@@ -141,45 +142,109 @@ void free_all(node* root)
 }
 
 int main(int argc , char* argv[]) {
+//    printf("%s\n",argv[3] );
+//    printf("%d\n",argc );
     node *root=getNode();
-    char word [word_size];
+    char *extend_word=NULL;
+    int size=0;
+    char *word=NULL;
+    word =(char*)malloc(1*sizeof(char));
+    short sizeofword= sizeof(*word);
+    char *index_for_free=word;
+    if (word==NULL){
+        printf("no allocet");
+        exit(1);
+    }
     char line[line_size];
     char c;
+    int max_len=1;
+    char *word_new=NULL;
     int len=0;
     boolean flag = FALSE;
     if(argc>=2 && strcmp(argv[1],"r")==0 ){
         flag=TRUE;
     }
-    while (fgets(line, sizeof(line),stdin)){
-        int counter=0;
-        len=strlen(line);
-        for (int i = 0; i <len ; i++) {
-            c=line[i];
-            if (c>='a' && c<='z' && c!= '\t' && c!=' ' && c!='\n' && c!='\0' ){
-                word[counter]=c;
-                counter++;
+    while ((c=getc(stdin))!=EOF){
+        if (c >= 'A' && c <= 'Z' && c != '\t' && c != ' ' && c != '\n' && c != '\0') {
+                char a = c + 32;
+                c = a;
             }
-            if(c>='A' && c<='Z' && c!= '\t' && c!=' ' && c!='\n' && c!='\0'){
-                char a= c+32;
-                c=a;
-                word[counter]=c;
-                counter++;
-            }
+        if (c >= 'a' && c <= 'z' && c != '\t' && c != ' ' && c != '\n' && c != '\0') {
 
-            if (c== '\t' || c==' ' || c=='\n' || c=='\0'){
-                word[counter]='\0';
-                insert(root,word);
-                counter=0;
-            }
+            if (len == word_size){
+                word_new=(char*)realloc(word, (size+word_size)* sizeof(char)); //+2 char
+                len+=word_size;
+                    if(word_new==NULL && word!=NULL){
+                        free(word);
+                        printf("not alocet");
+                        exit(1);
+                    }
+                    word =(char*)calloc(len, sizeof(char));
+                    strcpy(word,word_new);
+                    free(word_new);
+                    //max_len=len+2;
+                }
+            word[len]=c;
+                len++;
         }
+        if (c == '\t' || c == ' ' || c == '\n' || c == '\0'){
+            word[len]='\0';
+            insert(&root,word);
+        len=0;
+        }
+
     }
-    char str[word_size];
     if (flag){
-        printR(root,str,0);
+        printR(root,word,0);
     }else{
-        print(root,str,0);
+        print(root,word,0);
     }
     free_all(root);
+    free(word);
 
     return 0;
 }
+
+
+
+
+//    while (fgets(line, sizeof(line),stdin)) {
+//        int counter = 0;
+//        len = strlen(line);
+//        for (int i = 0; i < len; i++) {
+//            c = line[i];
+//            if (c >= 'a' && c <= 'z' && c != '\t' && c != ' ' && c != '\n' && c != '\0') {
+//                word[counter] = c;
+//                counter++;
+//            }
+//            if (c >= 'A' && c <= 'Z' && c != '\t' && c != ' ' && c != '\n' && c != '\0') {
+//                char a = c + 32;
+//                c = a;
+//                word[counter] = c;
+//                counter++;
+//            }
+//
+//            if (c == '\t' || c == ' ' || c == '\n' || c == '\0') {
+//                word[counter] = '\0';
+//                boolean flag = FALSE;
+//                for (int j = 0; j < strlen(word); j++) {
+//                    if (word[j] ==' ') {
+//                        flag = TRUE;
+//                    }
+//                }
+//                if (flag == FALSE) {
+//                    insert(root, word);
+//                    counter = 0;
+//                }
+//            }
+//        }
+//    }
+//
+//char str[word_size];
+//    if (flag){
+//        printR(root,str,0);
+//    }else{
+//        print(root,str,0);
+//    }
+//    free_all(root);
+
